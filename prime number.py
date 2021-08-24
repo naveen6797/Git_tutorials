dc504e4929f4223f85458a86212a526588370a8f
naveen6797 <73769278+naveen6797@users.noreply.github.com>
2021-08-24 08:50:29 +0530

    Update prime number.py

--- prime number.py
+++ prime number.py
@@ -1,6 +1,6 @@
-number = int(input('enter number:'))
-for i in range(2,number):
-    if number% i == 0:
+check_number = int(input('enter number:'))
+for i in range(2 , check_number):
+    if check_number % i == 0:
         print("it is not a prime number")
         break
 else:
